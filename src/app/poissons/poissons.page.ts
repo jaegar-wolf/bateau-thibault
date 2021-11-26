8c5d1d1c054c6f014a2e2cd04fc65022675e13bb
Adel MEDJDOUB <a.medjdoub@cfa-insta.fr>
2021-11-26 15:58:54 +0100

    Ajout de Toast et modif panier

--- src/app/poissons/poissons.page.ts
+++ src/app/poissons/poissons.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Produits } from 'src/models/produits';
 import { BateauThibaultService } from 'src/services/bateau-thibault.service';
 import { Storage } from '@ionic/storage';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-poissons',
@@ -13,7 +14,7 @@ export class PoissonsPage implements OnInit {
   produitsList: Produits[];
   produit: Produits[];
 
-  constructor(private router: Router, private bateauThibaultService: BateauThibaultService, private storage: Storage) { }
+  constructor(private router: Router, private bateauThibaultService: BateauThibaultService, private storage: Storage, private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.bateauThibaultService.getProduct().subscribe(res => {
@@ -29,9 +30,22 @@ export class PoissonsPage implements OnInit {
   saveToPanier(id: string, produit: Produits){
     console.log(produit);
     this.storage.set(id, produit);
+    this.openToast(produit)
   }
 
   goToPanier(){
     this.router.navigate(['/panier']);
+  }
+
+  async openToast(produit: Produits){
+    const toast = await this.toastCtrl.create({  
+      message: "Ajouté au panier",   
+      position: "bottom",
+      duration: 1000
+    });  
+    toast.present();
+    toast.onDidDismiss().then((val) => {  
+        console.log('Toast Dismissed');   
+    });  
   }
 }
