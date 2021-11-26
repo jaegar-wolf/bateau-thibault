413371e223b7626a48dd9d270b3717df7b3589fa
Adel MEDJDOUB <a.medjdoub@cfa-insta.fr>
2021-11-26 17:27:13 +0100

    Fomulaire de contact v0

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Component({
   selector: 'app-contact',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPage implements OnInit {
 
-  constructor() { }
+  constructor(private emailComposer: EmailComposer) { }
 
   ngOnInit() {
   }
@@ -16,5 +16,49 @@ export class ContactPage implements OnInit {
   goToFb(url : string){
     window.location.href=url;
   }
+
+  sendMailTo(app: any){
+    this.emailComposer.getClients().then((apps: []) => {
+      // Returns an array of configured email clients for the device
+   });
+   
+   this.emailComposer.hasClient().then(app, (isValid: boolean) => {
+    if (isValid) {
+      // Now we know we have a valid email client configured
+      // Not specifying an app will return true if at least one email client is configured
+    }
+   });
+   
+   this.emailComposer.hasAccount().then((isValid: boolean) => {
+    if (isValid) {
+      // Now we know we have a valid email account configured
+    }
+   });
+   
+   this.emailComposer.isAvailable().then(app, (available: boolean) => {
+    if(available) {
+      // Now we know we can send an email, calls hasClient and hasAccount
+      // Not specifying an app will return true if at least one email client is configured
+    }
+   });
+   
+   let email = {
+     to: 'max@mustermann.de',
+     cc: 'erika@mustermann.de',
+     bcc: ['john@doe.com', 'jane@doe.com'],
+     attachments: [
+       'file://img/logo.png',
+       'res://icon.png',
+       'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+       'file://README.pdf'
+     ],
+     subject: 'Cordova Icons',
+     body: 'How are you? Nice greetings from Leipzig',
+     isHtml: true
+   }
+   
+   // Send a text message using default options
+   this.emailComposer.open(email);
+  }  
 
 }
